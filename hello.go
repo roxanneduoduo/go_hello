b2c407caa347b97890180e44d0c75f7d7d00c369
roxanneduoduo <roxanne_duoduo@hotmail.com>
2018-12-03 15:04:15 +0800

    modified hello, import stringutil

--- hello.go
+++ hello.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/roxanneduoduo/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
 }
